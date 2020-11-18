1c6ac70c5199fdcf098ec23f3c47760d886b4bf7
webinzone <webinzone@gmail.com>
2020-11-18 11:40:10 +0530

    modified

--- index.js
+++ index.js
@@ -29,6 +29,17 @@ require("./config/strategy")(passport);
 app.get("/", (req, res) => {
   res.send("Hello");
 });
+   if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 
 //User connection and validation route
 app.use("/api/users", users);
